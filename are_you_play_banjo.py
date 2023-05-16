8115deb49b94308482e585f1e520b8bbe17e3ce2
raonieqr <raoniestevan@gmail.com>
2023-05-16 14:29:38 -0300

    are you play banjo

---
+++ are_you_play_banjo.py
@@ -0,0 +1,6 @@
+def are_you_playing_banjo(name):
+    if name[0] == 'R' or name[0] == 'r':
+        name = name + " plays banjo"
+    else:
+        name = name + " does not play banjo"
+    return name
