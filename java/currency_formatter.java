c98f6d50725cef722f1d2c59347c7effa854f1ca
raonieqr <raoniestevan@gmail.com>
2023-07-04 12:54:36 -0300

    add: currency_formatter java

---
+++ java/currency_formatter.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    double payment = scanner.nextDouble();
+    scanner.close();
+    
+    
+    System.out.println("US: " + NumberFormat.getCurrencyInstance(Locale.US).format(payment));
+    System.out.println("India: " + NumberFormat.getCurrencyInstance(new Locale("en", "IN")).format(payment));
+    System.out.println("China: " + NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment));
+    System.out.println("France: " + NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment));
+  }
+}
+
